23c603672636a9bfd54b12e4dab67a4fad3834b2
JValck <jasper_de_valck@hotmail.com>
2017-12-24 16:31:37 +0100

    gulp css

---
+++ Fos/gulpfile.js
@@ -0,0 +1,29 @@
+﻿/// <binding BeforeBuild='sass' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    fs = require('fs'),
+    less = require('gulp-less'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass');
+
+var paths = {
+    "node": "./node_modules/",
+    "webroot": "wwwroot/",
+    "cssAssets": "Resources/Assets/css/"
+}
+
+gulp.task('default', function () {
+    
+});
+
+gulp.task('sass', function () {
+    gulp.src(paths.cssAssets + "app.scss")
+        .pipe(sass())
+        .pipe(cssmin())
+        .pipe(gulp.dest(paths.webroot + "css"));
+});
